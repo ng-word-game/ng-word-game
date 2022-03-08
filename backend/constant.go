f29d3621bfed7d39b941bd6cae1b6e6cb50d4322
Kazuki Okamoto <kiki.com.start@gmail.com>
2022-03-09 02:00:49 +0900

    Backend: お題

--- backend/constant.go
+++ backend/constant.go
@@ -2,7 +2,7 @@ package main
 
 var roomMaxPlayer int = 2
 
-var themas []string = []string{"動物", "新宿", "渋谷", "お菓子", "バイト"}
+var themas []string = []string{"動物", "お菓子", "食べ物", "地名", "日用品", "飲み物", "乗り物"}
 
 const (
 	Initial      = iota
