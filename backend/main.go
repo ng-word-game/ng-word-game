fcf68e4242aab27ec23adb813ad6a68a79d0aaf9
Kazuki Okamoto <kiki.com.start@gmail.com>
2022-03-18 02:27:47 +0900

    Backend: logを書き出し

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
@@ -12,6 +14,7 @@ var (
 
 func main() {
 	flag.Parse()
+	loggingSettings("log")
 
 	mux := http.NewServeMux()
 	wsHandler := NewWshandler()
@@ -19,4 +22,17 @@ func main() {
 	mux.HandleFunc("/rooms", wsHandler.GetRooms)
 
 	log.Fatal(http.ListenAndServe(*addr, mux))
+}
+
+func loggingSettings(filename string) {
+	if f, err := os.Stat("./.logs"); os.IsNotExist(err) || !f.IsDir() {
+		os.Mkdir(".logs", 0777)
+	}
+	logfile, err := os.OpenFile("./.logs/" + filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println(err)
+	}
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	log.SetOutput(multiLogFile)
 }
