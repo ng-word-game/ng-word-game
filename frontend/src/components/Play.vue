6a92b2a30075195d32d1822ab2f82116bb291b7b
Kazuki Okamoto <kiki.com.start@gmail.com>
2022-04-18 23:13:15 +0900

    Revert "DEBUG: timerを表示"
    This reverts commit 6d4458acc0b841ec5eed0bd49e1d796bc0bbde4e.

--- frontend/src/components/Play.vue
+++ frontend/src/components/Play.vue
@@ -154,7 +154,6 @@ export default defineComponent({
     const timerDown = () => {
       timer.value = 60
       const id = setInterval(() => {
-        console.log('timer; ', timer.value)
         if (timer.value < 1) {
           ngChar.value = ''
           registerNgChar()
